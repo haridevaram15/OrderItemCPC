feea2c8861678cb82446d6737020a9728717396c
Chintalapudi <2064827@cognizant.com>
2022-03-07 21:00:32 +0530

    first commit

---
+++ Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderItem.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+        public int userId { get; set; }
+        public int menuitemId { get; set; }
+        public string menuitemName { get; set; }
+    }
+}
---
+++ Models/MenuItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderItem.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Free Delivery")]
+        public bool freeDelivery { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime dateOfLaunch { get; set; }
+
+        public bool Active { get; set; }
+
+    }
+}
